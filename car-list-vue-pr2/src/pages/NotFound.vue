2c590068571cc9203e40672b5998febc5604622a
Akash52 <ac8572611@gmail.com>
2022-03-30 01:37:13 +0530

    NotFound page added

---
+++ car-list-vue-pr2/src/pages/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center items-center">
+    <h1>Oops page not found 🙂</h1>
+    <p>
+      <router-link :to="{ name: 'Home' }">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Go to Home
+        </button>
+      </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
