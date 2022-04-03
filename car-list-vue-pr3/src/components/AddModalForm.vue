be0bca36ba4a25437c37fcf0212d149880b63ad3
Akash52 <ac8572611@gmail.com>
2022-04-03 20:36:45 +0530

    add modal form design done

--- car-list-vue-pr3/src/components/AddModalForm.vue
+++ car-list-vue-pr3/src/components/AddModalForm.vue
@@ -1,9 +1,16 @@
 <template>
+  <button
+    @click="isOpen = true"
+    class="px-6 py-2 mt-4 ml-8 mb-4 text-white bg-blue-600 rounded shadow text-right"
+    type="button"
+  >
+    Add New Car
+  </button>
   <form v-show="isOpen">
     <div
       class="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
     >
-      <div class="relative my-6 mx-auto max-w-4xl">
+      <div class="relative my-6 mx-auto w-full max-w-md">
         <div
           class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
         >
@@ -17,22 +24,26 @@
               </label>
               <input
                 type="text"
-                placeholder="CAR NAME eg. Audi A6"
+                placeholder="CAR NAME"
                 class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
               />
               <input
                 type="text"
-                placeholder="TOP SPEED eg. 200"
+                placeholder="TOP SPEED"
                 class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
               />
-              <input
-                type="text"
-                placeholder="PRICE eg. 100000"
+              <textarea
+                placeholder="DESCRIPTION"
                 class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
-              />
+              ></textarea>
               <input
                 type="text"
                 placeholder="IMAGE URL"
+                class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
+              />
+              <input
+                type="text"
+                placeholder="PRICE"
                 class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
               />
             </div>
@@ -60,13 +71,6 @@
     </div>
     <div class="opacity-25 fixed inset-0 z-40 bg-black"></div>
   </form>
-  <button
-    @click="isOpen = true"
-    class="px-6 py-2 text-white bg-blue-600 rounded shadow text-right"
-    type="button"
-  >
-    Open Model
-  </button>
 </template>
 
 <script>
@@ -86,7 +90,6 @@ export default {
         isOpen.value = true
       }
     })
-
     return { isOpen }
   }
 }
