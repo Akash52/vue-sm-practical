bda9faec76f700baa72388bf256c1ea90c8679a9
Akash52 <ac8572611@gmail.com>
2022-04-03 21:12:54 +0530

    bind input using v-model

--- car-list-vue-pr3/src/components/AddModalForm.vue
+++ car-list-vue-pr3/src/components/AddModalForm.vue
@@ -6,7 +6,7 @@
   >
     Add New Car
   </button>
-  <form v-show="isOpen">
+  <form v-show="isOpen" @submit.prevent="handleSubmit">
     <div
       class="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
     >
@@ -26,25 +26,30 @@
                 type="text"
                 placeholder="CAR NAME"
                 class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
+                v-model="name"
               />
               <input
                 type="text"
                 placeholder="TOP SPEED"
                 class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
+                v-model="top_speed"
               />
               <textarea
                 placeholder="DESCRIPTION"
                 class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
+                v-model="description"
               ></textarea>
               <input
                 type="text"
                 placeholder="IMAGE URL"
                 class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
+                v-model="image_link"
               />
               <input
                 type="text"
                 placeholder="PRICE"
                 class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
+                v-model="price"
               />
             </div>
           </div>
@@ -75,8 +80,30 @@
 
 <script>
 import { ref } from 'vue'
-// show modal on click
 export default {
+  // eslint-disable-next-line space-before-function-paren
+  data() {
+    return {
+      name: '',
+      top_speed: '',
+      description: '',
+      image_link: '',
+      price: ''
+    }
+  },
+  methods: {
+    // eslint-disable-next-line space-before-function-paren
+    handleSubmit() {
+      const newCar = {
+        name: this.name,
+        top_speed: this.top_speed,
+        description: this.description,
+        image_link: this.image_link,
+        price: this.price
+      }
+      console.log(newCar)
+    }
+  },
   // eslint-disable-next-line space-before-function-paren
   setup() {
     const isOpen = ref(false)
