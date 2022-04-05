5889e0817aa9103b6f1b88cd7f97c3eefe3fa687
Akash52 <ac8572611@gmail.com>
2022-04-05 10:28:26 +0530

    Header Add btn added

--- car-list-vue-pr3/src/components/HeaderComp.vue
+++ car-list-vue-pr3/src/components/HeaderComp.vue
@@ -32,16 +32,17 @@
             <router-link :to="{ name: 'AddCar' }">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="w-6 h-6"
+                class="w-6 h-6 transition duration-300 rounded-lg hover:text-slate-900 hover:scale-125"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
                 stroke-width="2"
+                title="Add new car"
               >
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
             </router-link>
