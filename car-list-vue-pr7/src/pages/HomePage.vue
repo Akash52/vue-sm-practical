a0e5ec7f1af4f3cc472989f4fe6f2175e42e13e0
Akash52 <ac8572611@gmail.com>
2022-05-17 18:22:51 +0530

    HomePage api move to vuex

--- car-list-vue-pr7/src/pages/HomePage.vue
+++ car-list-vue-pr7/src/pages/HomePage.vue
@@ -12,7 +12,7 @@
       :key="car.id"
       class="w-full max-w-sm px-4 py-6 mx-auto mb-4 transition duration-500 transform md:w-6/12 xl:4/12 lg:w-4/12 lg:mb-0 hover:scale-110"
     >
-      <transition appear="true" name="fade">
+      <transition name="fade">
         <CarList :car="car" @deleteCar="handleDelete" />
       </transition>
     </div>
@@ -23,42 +23,24 @@
 </template>
 
 <script>
-import CarList from '@/components/CarList.vue'
-import HeaderComp from '@/components/HeaderComp.vue'
-import axios from 'axios'
+import CarList from '@/components/CarList.vue';
+import HeaderComp from '@/components/HeaderComp.vue';
 
 export default {
   name: 'HomePage',
   // eslint-disable-next-line space-before-function-paren
-  data() {
-    return {
-      // here we are going to define our cars array
-      loading: false,
-      cars: []
-    }
+  created() {
+    this.$store.dispatch('getCars');
   },
-  // eslint-disable-next-line space-before-function-paren
-  mounted() {
-    this.loading = true
-    axios
-      .get('https://vue-fake-server.herokuapp.com/cardata')
-      .then((res) => {
-        this.cars = res.data
-        this.loading = false
-      })
-      .catch((err) => {
-        alert(err)
-      })
+  computed: {
+    // eslint-disable-next-line space-before-function-paren
+    cars() {
+      return this.$store.state.cars;
+    },
   },
 
-  methods: {
-    // eslint-disable-next-line space-before-function-paren
-    handleDelete(id) {
-      return (this.cars = this.cars.filter((car) => car.id !== id))
-    }
-  },
-  components: { CarList, HeaderComp }
-}
+  components: { CarList, HeaderComp },
+};
 </script>
 
 <style>
