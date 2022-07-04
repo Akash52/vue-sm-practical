ad6d7c054abd9a72b62955cc1e33b56f3a82cc04
Akash52 <ac8572611@gmail.com>
2022-07-04 14:10:27 +0530

    vuex modules

--- car-list-vue-pr7/src/store/index.js
+++ car-list-vue-pr7/src/store/index.js
@@ -1,75 +1,10 @@
-import { createStore } from 'vuex';
-import ApiService from '@/services/ApiService.js';
+import { createStore } from 'vuex'
+import cars from './modules/cars.js'
+import users from './modules/users.js'
 
 export default createStore({
-  state: {
-    cars: [],
-    users: [],
-  },
-  mutations: {
-    ADD_CAR(state, car) {
-      state.cars.push(car);
-    },
-    SET_CARS(state, cars) {
-      state.cars = cars;
-    },
-    DELETE_CAR(state, car) {
-      state.cars = state.cars.filter((c) => c.id !== car.id);
-    },
-    UPDATE_CAR(state, car) {
-      const index = state.cars.findIndex((c) => c.id === car.id);
-      state.cars.splice(index, 1, car);
-    },
-    ADD_USER(state, user) {
-      state.users.push(user);
-    },
-  },
-  actions: {
-    addCar({ commit }, car) {
-      ApiService.postCar(car)
-        .then(() => {
-          commit('ADD_CAR', car);
-          commit('SET_CARS', car);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    getCars({ commit }) {
-      ApiService.getCars()
-        .then((response) => {
-          commit('SET_CARS', response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    deleteCar({ commit }, car) {
-      ApiService.deleteCar(car)
-        .then(() => {
-          commit('DELETE_CAR', car);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    updateCar({ commit }, car) {
-      ApiService.putCar(car)
-        .then(() => {
-          commit('UPDATE_CAR', car);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    addUser({ commit }, car) {
-      ApiService.postUser(car)
-        .then(() => {
-          commit('ADD_USER', car);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-  },
-});
+  modules: {
+    cars: cars,
+    users: users
+  }
+})
